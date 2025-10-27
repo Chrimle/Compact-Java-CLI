a83553f1386abe60772bca21997be13daf940e14
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-10-28 00:17:28 +0100

    Add `Command` Annotation-class

---
+++ src/main/java/io/github/chrimle/compactjavacli/Command.java
@@ -0,0 +1,16 @@
+package io.github.chrimle.compactjavacli;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * A <em>method</em> may be annotated with <em>this</em> annotation.
+ * @since 0.1.0
+ * @author Chrimle
+ */
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface Command {}
